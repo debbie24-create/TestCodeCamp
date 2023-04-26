3d2603a0133559148670c8534155ee128f1d9b9d
debbie24-create <itohanidiaghe@yahoo.com>
2023-04-26 06:59:15 +0100

    Task 14 add FirstJavaProject to repository

---
+++ FirstJavaProject/src/com/debbiecreate/MainClass.java
@@ -0,0 +1,10 @@
+package com.debbiecreate;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Testify!");
+        System.out.println("This is my First Java Project.");
+        System.out.print("Java is among the top 3 programming languages");
+        System.out.print("It is an Object-oriented programming language");
+    }
+}
